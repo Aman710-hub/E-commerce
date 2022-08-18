249478c035407473d77def213e4d0698443eb895
Aman710-hub <amantur710@gmail.com>
2022-08-18 19:19:21 +0600

    build Error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <h4>error page</h4>
-}
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page is not found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
@@ -11,6 +21,7 @@ const Wrapper = styled.main`
   justify-content: center;
   align-items: center;
   text-align: center;
+
   h1 {
     font-size: 10rem;
   }
@@ -18,6 +29,6 @@ const Wrapper = styled.main`
     text-transform: none;
     margin-bottom: 2rem;
   }
-`
+`;
 
-export default ErrorPage
+export default ErrorPage;
