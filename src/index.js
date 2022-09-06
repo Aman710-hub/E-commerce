c669acaa1073f921075a434811360ba0c14ae696
Aman710-hub <amantur710@gmail.com>
2022-09-06 10:28:31 +0600

    setup Auth0 authentication

--- src/index.js
+++ src/index.js
@@ -8,14 +8,26 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// Domain
+// dev-mb0d74nd.us.auth0.com
+
+// client id
+// NVBv61dIllsHHusW3OR7RMc7UooatL1A
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-mb0d74nd.us.auth0.com"
+    clientId="NVBv61dIllsHHusW3OR7RMc7UooatL1A"
+    redirectUri={window.location.origin}
+    casheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
 
   document.getElementById("root")
 );
